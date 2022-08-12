3b824f3716a060e633fca1c67fef7b90b0f95df9
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-12 12:54:43 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <div>App</div>
+      <Footer />
+    </>
   )
 }
 
